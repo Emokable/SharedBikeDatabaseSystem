34b2f5db65c6efc0bb5caaac11837deceff0aedc
Emokable <424002709@qq.com>
2024-06-03 01:14:13 +0800

    问题已解决，目前图片路径在http://127.0.0.1:8080/images/下

--- sharedbike/src/main/java/com/example/sharedbike/Controller/FileController.java
+++ sharedbike/src/main/java/com/example/sharedbike/Controller/FileController.java
@@ -9,29 +9,23 @@ import java.io.IOException;
 
 @RestController
 public class FileController {
-    @PostMapping("/upload") //等价于RequestMapping(value="/upload",method=RequestMethod.POST)
-    //昵称 头像
-    public String up(String nickname, MultipartFile photo, HttpServletRequest request) throws IOException {
-        //昵称
-        System.out.println(nickname);
-        //获取图片的原始名称
-        System.out.println(photo.getOriginalFilename());
-        //取文件类型
-        System.out.println(photo.getContentType());
-        //获取web服务器的运行目录，因为最终要把文件存储到web服务器上（运行在linux系统上）
-        String path= request.getServletContext().getRealPath("/upload/");
-        System.out.println(path);
-        saveFile(photo,path);
-        return "上传成功！";
-    }
 
-    public void saveFile(MultipartFile photo, String path) throws IOException {
-        File dir= new File(path);
-        if(!dir.exists()){
-            dir.mkdir();
+        @PostMapping("/upload")
+        public String upload(String nickname, MultipartFile photo, HttpServletRequest request) throws IOException {
+            String path = request.getServletContext().getRealPath("/upload/");
+            saveFile(photo, path);
+            return "上传成功";
         }
-        File file = new File(path+photo.getOriginalFilename());
-        photo.transferTo(file);
+
+        public void saveFile(MultipartFile photo, String path) throws IOException {
+            File dir = new File(path);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+            File file = new File(path + photo.getOriginalFilename());
+            photo.transferTo(file);
+        }
+
 
     }
-}
+
