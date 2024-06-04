857dda6fadedb102cf34b49d914e15b28ab40c8e
unknown <2662680710@qq.com>
2024-06-05 02:44:33 +0800

    添加前端初始模版，增加部分依赖，后面开发用

--- sharedbike/src/main/java/com/example/sharedbike/sharebikeApplication.java
+++ sharedbike/src/main/java/com/example/sharedbike/sharebikeApplication.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan
 public class sharebikeApplication {
 
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         SpringApplication.run(sharebikeApplication.class, args);
     }
-
 }
