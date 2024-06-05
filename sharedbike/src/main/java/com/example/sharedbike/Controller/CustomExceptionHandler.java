65398aee04d40cb1fccc6d248c5578778155fd65
Emokable <424002709@qq.com>
2024-06-06 00:31:58 +0800

    使用shiro+session实现安全加密和权限管理

---
+++ sharedbike/src/main/java/com/example/sharedbike/Controller/CustomExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.example.sharedbike.Controller;
+
+import org.apache.shiro.authz.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+//返回无权限信息
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ErrorResponse> handleUnauthorizedException(UnauthorizedException ex) {
+        ErrorResponse errorResponse = new ErrorResponse("Access Denied", "You do not have permission to perform this action.");
+        return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+    }
+
+    static class ErrorResponse {
+        private String error;
+        private String message;
+
+        public ErrorResponse(String error, String message) {
+            this.error = error;
+            this.message = message;
+        }
+
+        public String getError() {
+            return error;
+        }
+
+        public void setError(String error) {
+            this.error = error;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+}
